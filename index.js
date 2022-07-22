55eacb264d0be9a47cf3893e49abc2626d84d8a8
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-07-22 15:12:43 -0500

    Sign up endpoints rendering form

--- index.js
+++ index.js
@@ -44,7 +44,20 @@ app.post('/signup', async (req, res) => {
 });
 
 app.get('/confirm', async (req, res) => {
-
+    try {
+        const contact = await getContactByEmail(req.query.email);
+        if(contact == null) throw `Contact not found.`;
+        if (contact.custom_fields.conf_num ==  req.query.conf_num) {
+          const listID = await getListID('Newsletter Subscribers');
+          await addContactToList(req.query.email, listID);
+        } else {
+          throw 'Confirmation number does not match';
+        }
+        res.render('message', { message: 'You are now subscribed to our newsletter. We can\'t wait for you to hear from us!' });
+      } catch (error) {
+        console.error(error);
+        res.render('message', { message: 'Subscription was unsuccessful. Please <a href="/signup">try again.</a>' });
+      }
 });
 
 app.get('/upload', (req, res) => {
@@ -58,9 +71,9 @@ app.get('/delete', async (req, res) => {
 
 });
 
-// Helper functions and variables
 
 // Helper functions and variables
+
 const signUpPage = {
     title: 'Join Our Newsletter',
     subtitle: 'Subscribe to our newsletter to receive the latest news and products.',
@@ -113,6 +126,45 @@ const signUpPage = {
     const response = await sgClient.request(request);
     const allCustomFields = response[1].custom_fields;
     return allCustomFields.find(x => x.name === customFieldName).id;
+   }
+
+   async function getContactByEmail(email) {
+    const data = {
+      "emails": [email]
+    };
+    const request = {
+      url: `/v3/marketing/contacts/search/emails`,
+      method: 'POST',
+      body: data
+    }
+    const response = await sgClient.request(request);
+    if(response[1].result[email]) return response[1].result[email].contact;
+    else return null;
+   }
+   
+   async function getListID(listName) {
+    const request = {
+      url: `/v3/marketing/lists`,
+      method: 'GET',
+    }
+    const response = await sgClient.request(request);
+    const allLists = response[1].result;
+    return allLists.find(x => x.name === listName).id;
+   }
+   
+   async function addContactToList(email, listID) {
+    const data = {
+      "list_ids": [listID],
+      "contacts": [{
+        "email": email
+      }]
+    };
+    const request = {
+      url: `/v3/marketing/contacts`,
+      method: 'PUT',
+      body: data
+    }
+    return sgClient.request(request);
    }
    
 
